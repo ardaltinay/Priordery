6cc581e83fa384326dc929c4a63a11615c923f1c
ardaltinay <arda.altinay@ozguryazilim.com.tr>
2024-08-08 16:41:29 +0300

    initial commit

---
+++ src/main/java/com/ardaltinay/Priordery/PriorderyApplication.java
@@ -0,0 +1,13 @@
+package com.ardaltinay.Priordery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorderyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriorderyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ardaltinay/Priordery/PriorderyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardaltinay.Priordery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriorderyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
